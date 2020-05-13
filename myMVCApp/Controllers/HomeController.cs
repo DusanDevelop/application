951b18b4f1a3d305d5cf5faa99662e00cd597e6f
Dusan <dusan.revaj@globallogic.com>
2020-05-13 21:07:05 +0200

    dalsia zmena

--- myMVCApp/Controllers/HomeController.cs
+++ myMVCApp/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-// Zmena v homeMaster branch DusanBranch
+// Zmena v homeMaster branch DusanBranch dalsia zmena
 namespace myMVCApp.Controllers
 {
     public class HomeController : Controller
