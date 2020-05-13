d88c58eb3d7e2484d8e5382a59d17a8785cced5e
Dusan <dusan.revaj@globallogic.com>
2020-05-13 21:02:43 +0200

    dusanbranch zmeny

--- myMVCApp/Controllers/HomeController.cs
+++ myMVCApp/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-// Zmena v homeMaster branch
+// Zmena v homeMaster branch DusanBranch
 namespace myMVCApp.Controllers
 {
     public class HomeController : Controller
